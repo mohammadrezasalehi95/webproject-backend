fe6ab84b1cea5f9d632421a9d9990cb42e9b975f
sadegh khorasani <mohammadsakh@gmail.com>
2019-01-25 13:33:34 +0330

    add some models

--- sport/models.py
+++ sport/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Team(models.Model):
+    name=models.CharField(max_length=20,primary_key=True)
+    bio=models.TextField(max_length=500)
+
+
+class Person(models.Model):
+
+    pid=models.IntegerField()
+    name=models.CharField(max_length=20)
+    bio=models.TextField(max_length=500)
+    gender=models.CharField(max_length=5)
+    image=models.ImageField(upload_to='assets/')
+    birthDay=models.DateField()
+    age=models.IntegerField()
+    height=models.IntegerField()
+    weight=models.IntegerField()
+    currentTeam=models.ForeignKey(Team,on_delete=models.CASCADE,to_field=name)
+    national=models.CharField(max_length=20)
+    rule=models.CharField(max_length=20)
+
+
+
+class New(models.Model):
+    title=models.TextField(max_length=500)
+    subtitle=models.TextField(max_length=500)
+
+
+
