7d428094d588e6741917cdb45c68ee3aa191db10
sadegh khorasani <mohammadsakh@gmail.com>
2019-01-25 17:50:10 +0330

    add urls

---
+++ webProject/sport/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from webProject.sport.views import GameResults
+
+urlpatterns = [
+    path('GameResults/(?P<teamName>\w{0,50})',GameResults.as_view()),
+
+]
