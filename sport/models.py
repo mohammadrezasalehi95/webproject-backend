ae649802215122f3e67293829fc723a3dbfd1a42
sadegh khorasani <mohammadsakh@gmail.com>
2019-01-25 15:15:59 +0330

    add game models

--- sport/models.py
+++ sport/models.py
@@ -14,7 +14,7 @@ class Person(models.Model):
     name=models.CharField(max_length=20)
     bio=models.TextField(max_length=500)
     gender=models.CharField(max_length=5)
-    image=models.ImageField(upload_to='assets/')
+    image=models.ImageField(upload_to='assets/players')
     birthDay=models.DateField()
     age=models.IntegerField()
     height=models.IntegerField()
@@ -22,12 +22,46 @@ class Person(models.Model):
     currentTeam=models.ForeignKey(Team,on_delete=models.CASCADE,to_field=name)
     national=models.CharField(max_length=20)
     rule=models.CharField(max_length=20)
+    previousClub=models.CharField(max_length=20)
+    squad=models.CharField(max_length=20)
+
 
 
 
 class New(models.Model):
     title=models.TextField(max_length=500)
     subtitle=models.TextField(max_length=500)
+    image=models.ImageField(upload_to='assets/teams')
 
 
 
+class Game(models.Model):
+    team1=models.ForeignKey(Team,on_delete=models.CASCADE)
+    team2=models.ForeignKey(Team,on_delete=models.CASCADE)
+    date=models.DateField()
+    status=models.IntegerField()
+    team1_score=models.IntegerField()
+    team2_score=models.IntegerField()
+    team1_possession=models.IntegerField()
+    team2_possession=models.IntegerField()
+    team1_shots=models.IntegerField()
+    team2_shots=models.IntegerField()
+    team1_corner=models.IntegerField()
+    team2_corner=models.IntegerField()
+
+
+class Game_Player(models.Model):
+    game=models.ForeignKey(Game,on_delete=models.CASCADE)
+    name=models.CharField(max_length=20)
+    playerNumber=models.CharField(max_length=20)
+    post=models.CharField(max_length=20)
+    changingTime=models.CharField(max_length=20)
+
+class Game_Report(models.Model):
+    game=models.ForeignKey(Game,on_delete=models.CASCADE,primary_key=True)
+    last_report=models.TextField(max_length=500)
+
+class Game_Event(models.Model):
+    game=models.ForeignKey(Game,on_delete=models.CASCADE)
+    time=models.DateTimeField()
+    text=models.TextField()
